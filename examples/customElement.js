37d937117be909d0e988e87a74e369d72c2347cc
brian <bkardell@igalia.com>
2019-06-11 10:44:44 -0400

    update to custom elements/shadow dom v1

--- examples/customElement.js
+++ examples/customElement.js
@@ -2,55 +2,54 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 /*global TeXZilla, Element, document*/
-
-(function () {
+(function() {
     "use strict";
 
     function updateMathMLOutput(aElement) {
-        var tex = aElement.textContent,
-            display = aElement.getAttribute("display"),
-            dir = aElement.getAttribute("dir");
-        try {
-            // Parse the TeX input and replace it with the MathML output.
-            aElement.shadowRoot.innerHTML =
-                TeXZilla.toMathMLString(tex, display === "block",
-                                        dir === "rtl", true);
-        } catch (e) {
-            // Parsing failed: use an <merror> with the original TeX input.
-            aElement.shadowRoot.innerHTML =
-                "<math><merror><mtext>" + tex + "</mtext></merror></math>";
-        }
+      var tex = aElement.textContent,
+        display = aElement.getAttribute("display"),
+        dir = aElement.getAttribute("dir");
+      try {
+        // Parse the TeX input and replace it with the MathML output.
+        aElement.shadowRoot.innerHTML = TeXZilla.toMathMLString(
+          tex,
+          display === "block",
+          dir === "rtl",
+          true
+        );
+      } catch (e) {
+        // Parsing failed: use an <merror> with the original TeX input.
+        aElement.shadowRoot.innerHTML =
+          "<math><merror><mtext>" + tex + "</mtext></merror></math>";
+      }
     }
 
-    var XTeXProto = Object.create(Element.prototype, {
-        source: {
-            // Implement a "source" property to get/set the TeX source.
-            get: function () {
-                return this.textContent;
-            },
-            set: function (aTeX) {
-                this.textContent = aTeX;
-                updateMathMLOutput(this);
-            }
-        }
-    });
-
-    XTeXProto.createdCallback = function () {
-        this.createShadowRoot();
+    class XTexElement extends HTMLElement {
+      get source() {
+        return this.textContent;
+      }
+  
+      set source(aTeX) {
+        this.textContent = aTeX;
         updateMathMLOutput(this);
-    };
+      }
+  
+      connectedCallback() {
+        this.attachShadow({ mode: "open" });
+        updateMathMLOutput(this);
+      }
 
-    XTeXProto.attributeChangedCallback = function (aName, aOld, aNew) {
+      attributeChangedCallback(aName, aOld, aNew) {
         if (aName === "dir" || aName === "display") {
-            if (aNew === null) {
-                this.shadowRoot.firstElementChild.removeAttribute(aName);
-            } else {
-                this.shadowRoot.firstElementChild.setAttribute(aName, aNew);
-            }
+          if (aNew === null) {
+            this.shadowRoot.firstElementChild.removeAttribute(aName);
+          } else {
+            this.shadowRoot.firstElementChild.setAttribute(aName, aNew);
+          }
         }
-    };
+      }
+    } 
 
-    document.registerElement("x-tex", {
-        prototype: XTeXProto
-    });
-}());
+    customElements.define("x-tex", XTexElement);
+  })();
+  
